4cdcbfef98aaf55e7c82cd0758e80f325b4acb2f
danielglez4234 <gnzlzdaniel.task@gmail.com>
2019-12-16 17:56:19 +0000

    setting app | creating components |  install router, axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/Nav';
+import SearchForm from './components/SearchForm';
+import Gallery from './components/Gallery';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <SearchForm />
+      <Nav />
+      <Gallery />
     </div>
   );
 }
---
+++ src/components/Gallery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GalleryItem from './GalleryItem';
+
+const Gallery = () => {
+  return(
+    <div class="photo-container">
+      <h2>Results</h2>
+      <GalleryItem />
+    </div>
+  );
+}
+export default Gallery;
